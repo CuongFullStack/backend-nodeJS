59fe78b4f4b8f89e18915b7f75c216118b21eb0f
Vu Cuong <vuvietcuong1351998@gmail.com>
2023-01-10 15:17:10 +0700

    video30 test and connection to db

--- src/server.js
+++ src/server.js
@@ -2,8 +2,9 @@ require("dotenv").config();
 const express = require("express"); //commonjs
 const configViewEngine = require("./config/viewEngine");
 const webRoutes = require("./routes/web");
+const mysql = require("mysql2");
 
-const app = express(); //app
+const app = express(); //app express
 const port = process.env.PORT || 8888; //port => hardcode .uat .prod
 const hostname = process.env.HOST_NAME;
 
@@ -14,6 +15,22 @@ configViewEngine(app);
 //Khai báo route
 app.use("/v1", webRoutes);
 app.use("/v2", webRoutes);
+
+//test connection
+//create the connection to database
+const connection = mysql.createConnection({
+  host: "localhost",
+  port: 3307, // default port 3306
+  user: "root", // default password: empty
+  password: "123456",
+  database: "hoidanit",
+});
+
+// simple query
+connection.query("select * from Users u;", function (err, results, fields) {
+  console.log(">>>results=", results); // results contains rows returned by server
+  console.log(">>>fields=", fields); // fields contains extra meta data about results, if available
+});
 
 app.listen(port, hostname, () => {
   console.log(`Example app listening on port ${port}`);
