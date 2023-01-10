775a73c2b50695b85ced6b519f80ab17982be0a6
Vu Cuong <vuvietcuong1351998@gmail.com>
2023-01-10 15:54:19 +0700

    video36

--- src/server.js
+++ src/server.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 const express = require("express"); //commonjs
 const configViewEngine = require("./config/viewEngine");
 const webRoutes = require("./routes/web");
-const mysql = require("mysql2");
+const connection = require("./config/database");
 
 const app = express(); //app express
 const port = process.env.PORT || 8888; //port => hardcode .uat .prod
@@ -16,20 +16,11 @@ configViewEngine(app);
 app.use("/v1", webRoutes);
 app.use("/v2", webRoutes);
 
-//test connection
-//create the connection to database
-const connection = mysql.createConnection({
-  host: "localhost",
-  port: 3307, // default port 3306
-  user: "root", // default password: empty
-  password: "123456",
-  database: "hoidanit",
-});
-
 // simple query
-connection.query("select * from Users u;", function (err, results, fields) {
+connection.query("select * from Users u", function (err, results, fields) {
+  // console.log(err);
   console.log(">>>results=", results); // results contains rows returned by server
-  console.log(">>>fields=", fields); // fields contains extra meta data about results, if available
+  // console.log(">>>fields=", fields); // fields contains extra meta data about results, if available
 });
 
 app.listen(port, hostname, () => {
