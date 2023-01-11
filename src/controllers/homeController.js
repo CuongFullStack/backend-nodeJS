516b56ae5cd058a14aeb96c8753a20cffa59844a
Vu Cuong <vuvietcuong1351998@gmail.com>
2023-01-11 22:04:02 +0700

    video49 Get User By Id

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,5 +1,5 @@
 const connection = require("../config/database");
-const { getAllUsers } = require("../services/CRUDService");
+const { getAllUsers, getUserById } = require("../services/CRUDService");
 
 const getHomepage = async (req, res) => {
   let results = await getAllUsers();
@@ -32,10 +32,12 @@ const getCreatePage = (req, res) => {
   res.render("create.ejs");
 };
 
-const getUpdatePage = (req, res) => {
+const getUpdatePage = async (req, res) => {
   const userId = req.params.id;
-  console.log(">>> req.params::", req.params, userId);
-  return res.render("edit.ejs", {});
+
+  let user = await getUserById(userId);
+
+  return res.render("edit.ejs", { userEdit: user });
 };
 
 module.exports = {
