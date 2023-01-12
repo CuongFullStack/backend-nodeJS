77fe0973ec511a58e6a4746606573890b13fdad5
Vu Cuong <vuvietcuong1351998@gmail.com>
2023-01-12 20:30:39 +0700

    video64  Connection Options

--- src/server.js
+++ src/server.js
@@ -19,9 +19,14 @@ configViewEngine(app);
 // Khai báo route
 app.use("/", webRoutes);
 
-//Test connection
-connection();
-
-app.listen(port, hostname, () => {
-  console.log(`Example app listening on port ${port}`);
-});
+(async () => {
+  //Test connection
+  try {
+    await connection();
+    app.listen(port, hostname, () => {
+      console.log(`Backend zero app listening on port ${port}`);
+    });
+  } catch (error) {
+    console.log(">>> Error connect to DB: ", error);
+  }
+})();
