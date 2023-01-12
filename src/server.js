aa5e9e7b2731c4b1fa29921eec7ac093d8b94697
Vu Cuong <vuvietcuong1351998@gmail.com>
2023-01-12 20:56:24 +0700

    video65 Create Database

--- src/server.js
+++ src/server.js
@@ -3,6 +3,7 @@ const express = require("express"); //commonjs
 const configViewEngine = require("./config/viewEngine");
 const webRoutes = require("./routes/web");
 const connection = require("./config/database");
+const mongoose = require("mongoose");
 
 const app = express(); //app express
 const port = process.env.PORT || 8888; //port => hardcode .uat .prod
@@ -18,6 +19,13 @@ configViewEngine(app);
 
 // Khai báo route
 app.use("/", webRoutes);
+
+const kittySchema = new mongoose.Schema({
+  name: String,
+});
+const Kitten = mongoose.model("Kitten", kittySchema);
+const cat = new Kitten({ name: "Hoi Dan IT cat" });
+cat.save();
 
 (async () => {
   //Test connection
